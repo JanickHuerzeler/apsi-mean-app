6a64b34d3be25f08e7f9ddbc11cc60b2a7d2ad4d
Janick Hürzeler <janick.huerzeler@gmail.com>
2021-01-02 18:10:04 +0100

    Connect to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const bodyparser = require("body-parser");
 const app = express();
+const postmodel = require('./models/post');
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://apsi-tutorial-user:jhNxlVW8VjPurTpD@cluster0.9qznl.mongodb.net/posts?retryWrites=true&w=majority").then(() => {
+  console.log("Connected to database");
+})
+  .catch(() => {
+    console.log("Connection Failed");
+  });
 
 app.use(bodyparser.json());
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
   // Could use more restricted header values
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Methods",
+  res.setHeader("Access-Control-Allow-Methods",
     "GET, POST, PATCH, DELETE, OPTIONS");
   next();
 });
@@ -32,14 +41,14 @@ app.get('/api/posts', (req, res, next) => {
       title: 'Third server-side post',
       content: 'This is comming from the server'
     }];
-    res.status(200).json({
-      message: 'Posts Fetched Successfully',
-      posts: posts
-    });
+  res.status(200).json({
+    message: 'Posts Fetched Successfully',
+    posts: posts
+  });
 });
 
-app.post("/api/posts",(req, res, next)=>{
-  const post = req.body;
+app.post("/api/posts", (req, res, next) => {
+  const post = new postmodel({ title: req.body.title, content: req.body.content });
   console.log(post);
   res.status(201).json({
     message: 'Post added successfully'
