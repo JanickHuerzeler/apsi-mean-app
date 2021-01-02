d7b6120e6a6981e071787be8b08343c8de601087
Janick Hürzeler <janick.huerzeler@gmail.com>
2021-01-02 16:36:27 +0100

    Backend JSON response

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First Middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'snkfkjkf',
+      title: 'First server-side post',
+      content: 'This is comming from the server'
+    },
+    {
+      id: 'gyrrshjhk',
+      title: 'Second server-side post',
+      content: 'This is commiing from the server'
+    },
+    {
+      id: 'mhsetghj',
+      title: 'Third server-side post',
+      content: 'This is comming from the server'
+    }];
+    res.status(200).json({
+      message: 'Posts Fetched Successfully',
+      posts: posts
+    });
 });
 
 module.exports = app;
